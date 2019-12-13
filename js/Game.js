be2e8b2fff2683a883200751ce41d1b5f14bea67
Aaron Yuan <aaronyuan27@gmail.com>
2019-12-13 01:39:10 -0700

    updated game logic

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,43 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game{
+    constructor(){
+        this.missed = 0;
+        this.activePhrase = null;
+        this.phrases = [
+            new Phrase("Miss the boat"),
+            new Phrase("Once in a blue moon"),
+            new Phrase("Beat around the bush"),
+            new Phrase("Back to the drawing board"),
+            new Phrase("Break a leg"),
+
+        ];
+    }
+    startGame(){
+
+    }
+
+    getRandomPhrase(){
+        const num = Math.floor(Math.random()*(this.phrases.length + 1));
+        return this.phrases[num].phrase;
+    }
+
+    handleInteraction(){
+        
+    }
+
+    removeLife(){
+        const selectedLi = document.getElementsByClassName("tries").selectedIndex = "missed";
+        selectedLi.src = "../images/lostHeart.png";
+        missed++;
+
+        if(missed === 5){
+           gameOver();
+        }
+    }
+
+    checkForWin(){
+        let hiddenLis = document.querySelectorAll(".hide");
+        if(hiddenLis.length === 0){
+            return true;
+        }
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,43 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase{
+
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+    
+    addPhraseToDisplay(){
+        const phraseLetters = [...this.phrase];
+        const ul = document.querySelector("#phrase ul");
+        phraseLetters.forEach(phraseLetter => {
+            const li = document.createElement("li");
+            li.innerHTML= phraseLetter;
+            if(li.innerHtml != ""){
+                li.className +=`hide letter ${phraseLetter}`;
+            }
+            else{
+                li.className +="space";
+            }
+            
+            ul.appendChild(li);
+        });
+    }
+
+    checkLetter(char){
+        const phraseLis = document.querySelector("#phrase li");
+        phraseLis.forEach(li =>{
+            if(char === li.textContent()){
+                return true;
+            }
+            else return false;
+        });
+   } 
+}
+
+    showMatchedLetter(char){
+        const phraseLis = document.querySelector("#phrase li");
+        phraseLis.forEach(li =>{
+            if(char===li.textContent){
+                li.className += "show";
+                li.className -= "hide";
+            }
+        });
+    }
